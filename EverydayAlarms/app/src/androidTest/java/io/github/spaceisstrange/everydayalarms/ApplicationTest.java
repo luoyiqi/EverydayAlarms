04a67b2630da9056e66d8730f0651c9f1039b8c5
Fran González <zox198@gmail.com>
2016-05-23 16:04:02 +0200

    Initial commit of the project

---
+++ EverydayAlarms/app/src/androidTest/java/io/github/spaceisstrange/everydayalarms/ApplicationTest.java
@@ -0,0 +1,10 @@
+package io.github.spaceisstrange.everydayalarms;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
