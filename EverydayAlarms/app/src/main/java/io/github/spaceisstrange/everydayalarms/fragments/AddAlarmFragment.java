0708a21adcc2879c5461ec3034b5a78e9aa156b6
Fran González <zox198@gmail.com>
2016-05-29 20:35:08 +0200

    Hour dialog picker added

--- EverydayAlarms/app/src/main/java/io/github/spaceisstrange/everydayalarms/fragments/AddAlarmFragment.java
+++ EverydayAlarms/app/src/main/java/io/github/spaceisstrange/everydayalarms/fragments/AddAlarmFragment.java
@@ -1,25 +1,37 @@
 package io.github.spaceisstrange.everydayalarms.fragments;
 
-import android.support.v4.app.Fragment;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 import java.util.Calendar;
 
 import io.github.spaceisstrange.everydayalarms.R;
+import io.github.spaceisstrange.everydayalarms.dialogs.HourPickerDialog;
 import io.github.spaceisstrange.everydayalarms.model.Alarm;
 import io.github.spaceisstrange.everydayalarms.model.enums.Day;
+import io.github.spaceisstrange.everydayalarms.util.AlarmUtils;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class AddAlarmFragment extends Fragment {
-
+public class AddAlarmFragment extends Fragment implements TimePickerDialog.OnTimeSetListener {
+    /**
+     * The selected hour of the alarm
+     */
+    private int mAlarmHour;
+    /**
+     * The selected minute of the alarm
+     */
+    private int mAlarmMinute;
     /**
      * Views of the fragment
      */
@@ -29,10 +41,22 @@ public class AddAlarmFragment extends Fragment {
     private LinearLayout mBtnAlarmVibration;
     private Switch mSwAlarmVibration;
     private SeekBar mSbAlarmSound;
+    private TextView mTvAlarmTime;
+
+    /**
+     * The tag of the dialogs to be shown
+     */
+    public static final String HOUR_PICKER_TAG = "hourPicker";
 
     public static AddAlarmFragment newInstance() {
-        return new AddAlarmFragment();
+        AddAlarmFragment alarmFragment = new AddAlarmFragment();
+        alarmFragment.setAlarmTimeToNow();
+        return alarmFragment;
     }
+
+    /*
+    METHODS
+     */
 
     /**
      * Creates the alarm based on the selected preferences from the user
@@ -41,9 +65,28 @@ public class AddAlarmFragment extends Fragment {
      */
     public Alarm getAlarmCreated() {
         // TODO: Create a real alarm here
-        return new Alarm(0, Day.FRIDAY, Calendar.getInstance(), true, true, 90);
+        return new Alarm(0, Day.FRIDAY, mAlarmHour, mAlarmMinute, true, false, 100);
     }
 
+    /**
+     * Sets the current alarm time to right now
+     */
+    public void setAlarmTimeToNow() {
+        Calendar calendar = Calendar.getInstance();
+        mAlarmHour = calendar.get(Calendar.HOUR_OF_DAY);
+        mAlarmMinute = calendar.get(Calendar.MINUTE);
+    }
+
+    /**
+     * Updates the current time of the alarm
+     */
+    public void updateAlarmTime() {
+        mTvAlarmTime.setText(getString(R.string.add_alarm_time_format, mAlarmHour, AlarmUtils.formatMinutes(mAlarmMinute)));
+    }
+
+    /*
+    OVERRIDES
+     */
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -56,9 +99,28 @@ public class AddAlarmFragment extends Fragment {
         mBtnAlarmVibration = (LinearLayout) v.findViewById(R.id.fragment_add_alarm_btn_alarm_vibration);
         mSwAlarmVibration = (Switch) v.findViewById(R.id.fragment_add_alarm_sw_alarm_vibration);
         mSbAlarmSound = (SeekBar) v.findViewById(R.id.fragment_add_alarm_sb_alarm_volume);
+        mTvAlarmTime = (TextView) v.findViewById(R.id.fragment_add_alarm_tv_alarm_time);
+
+        // Update the current time of the alarm time text view
+        updateAlarmTime();
 
         // TODO: Set the action for every button
 
+        // Set the action of the alarm time button
+        mBtnAlarmTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                HourPickerDialog.newInstance(AddAlarmFragment.this).show(getFragmentManager(), HOUR_PICKER_TAG);
+            }
+        });
+
         return v;
+    }
+
+    @Override
+    public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+        mAlarmHour = hourOfDay;
+        mAlarmMinute = minute;
+        updateAlarmTime();
     }
 }
