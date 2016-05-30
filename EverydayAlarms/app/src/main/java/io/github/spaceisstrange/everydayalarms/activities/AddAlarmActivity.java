fa4d4e34df0c9be83c3808cde3699dd95d3cfcc3
Fran González <zox198@gmail.com>
2016-05-30 03:09:06 +0200

    Cleanup

--- EverydayAlarms/app/src/main/java/io/github/spaceisstrange/everydayalarms/activities/AddAlarmActivity.java
+++ EverydayAlarms/app/src/main/java/io/github/spaceisstrange/everydayalarms/activities/AddAlarmActivity.java
@@ -4,14 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import io.github.spaceisstrange.everydayalarms.R;
 import io.github.spaceisstrange.everydayalarms.fragments.AddAlarmFragment;
-import io.github.spaceisstrange.everydayalarms.model.Alarm;
 import io.github.spaceisstrange.everydayalarms.util.FragmentSwitcher;
 
 public class AddAlarmActivity extends AppCompatActivity {
